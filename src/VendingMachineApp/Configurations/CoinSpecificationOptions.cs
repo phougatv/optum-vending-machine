b11c4136356420e5160dfa5f4869e78a79116427
Vedant Phougat <9568209+phougatv@users.noreply.github.com>
2025-06-23 00:12:06 +0530

    initial changes

---
+++ src/VendingMachineApp/Configurations/CoinSpecificationOptions.cs
@@ -0,0 +1,18 @@
+﻿namespace Optum.VendingMachineApp.Configurations;
+
+public class CoinSpecificationOptions
+{
+	public String CoinType { get; set; }
+	public Decimal MonetaryValue { get; set; }
+	public Double WeightInGrams { get; set; }
+	public Double DiameterInMillimeters { get; set; }
+	public Double Tolerance { get; set; } = 0.001;
+}
+
+public class ProductOptions
+{
+	public string Name { get; set; }
+	public decimal Price { get; set; }
+	public int InitialQuantity { get; set; }
+}
+
